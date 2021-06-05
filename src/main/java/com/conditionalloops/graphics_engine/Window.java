4d2271bf21a172f80ce024ac1235c507746cdd00
Your Name <you@example.com>
2021-06-05 02:28:45 +0100

    added clean up code to remove resources.

--- src/main/java/com/conditionalloops/graphics_engine/Window.java
+++ src/main/java/com/conditionalloops/graphics_engine/Window.java
@@ -1,5 +1,6 @@
 package com.conditionalloops.graphics_engine;
 
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
 import static org.lwjgl.glfw.GLFW.GLFW_MAXIMIZED;
 import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
@@ -7,12 +8,15 @@ import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
 import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
 import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
 import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
 import static org.lwjgl.glfw.GLFW.glfwInit;
 import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
 import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
 import static org.lwjgl.glfw.GLFW.glfwShowWindow;
 import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
 import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
 import static org.lwjgl.glfw.GLFW.glfwWindowHint;
 import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
@@ -24,7 +28,6 @@ import java.util.Objects;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -32,7 +35,7 @@ import org.slf4j.LoggerFactory;
  */
 public class Window {
 
-  private final static Logger Logger = LoggerFactory
+  private final static org.slf4j.Logger Logger = LoggerFactory
       .getLogger(com.conditionalloops.graphics_engine.Window.class);
   private static final Object Lock = new Object();
 
@@ -73,7 +76,20 @@ public class Window {
         Version.getVersion());
     init();
     loop();
+    clean();
   }
+
+  private void clean() {
+    // Free the window callbacks and destroy the window
+    glfwFreeCallbacks(glfwWindow);
+    glfwDestroyWindow(glfwWindow);
+
+    // Terminate GLFW and free the error callback
+    glfwTerminate();
+    glfwSetErrorCallback(null).free();
+  }
+
+
 
   private void init() {
     GLFWErrorCallback.createPrint(System.err).set();
